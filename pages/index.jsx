bdb4a84a0425469bec507e77115069e635b200ce
Pablo Ibarra <pabloo_ibarra98@hotmail.com>
2022-08-05 11:49:10 -0500

    initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,42 @@
+import { useMoralisQuery, useMoralis } from "react-moralis";
+import { NFTBox } from "../components/NFTBox";
+
+export default function Home() {
+  const { isWeb3Enabled } = useMoralis();
+  const { data: listedNfts, isFetching: fetchingListedNfts } = useMoralisQuery(
+    "ActiveItem",
+    query => query.limit(10).descending("tokenId")
+  );
+
+  return (
+    <div className="container mx-auto">
+      <h1 className="py-4 px-4 font-bold text-2xl">Recently Listed</h1>
+      <div className="flex flex wrap">
+        {
+          isWeb3Enabled ? (
+            fetchingListedNfts ? (
+              <div>Loading...</div>
+            ): (
+              listedNfts.map((nft) => {
+                console.log(nft.attributes)
+                const { price, nftAddress, tokenId, marketplaceAddress, seller } =
+                  nft.attributes
+                return (
+                  <NFTBox
+                    price={price}
+                    nftAddress={nftAddress}
+                    tokenId={tokenId}
+                    marketplaceAddress={marketplaceAddress}
+                    seller={seller}
+                    key={`${nftAddress}${tokenId}`}
+                  />
+                )
+              })
+            )
+          ) : (
+            <div>Web3 Currently Not Enabled</div>
+          )}
+      </div>
+    </div>
+  )
+}
